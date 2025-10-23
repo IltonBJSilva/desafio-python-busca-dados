45f7c2419aa75eecf1e030d5e31b4fe807d6c9e6
Ilton Batista <xlsonplay@gmail.com>
2025-10-23 14:04:46 -0300

    feat: Configura conexão inicial com o banco de dados via SQLAlchemy

---
+++ app/database/connection.py
@@ -0,0 +1,8 @@
+# app/database/connection.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
